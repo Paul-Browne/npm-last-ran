909a62e5668e203110477945e78ffc08a93e2a0a
paul browne <paulmatthewbrowne@gmail.com>
2021-02-17 15:15:37 +0200

    refactors unique string to use node internals

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-const _path = id => path.join(__dirname, "_" + (id||0).toString());
+const _path = id => {
+	const e = new Error();
+	// do match first
+	const matched = e.stack.match(/node_modules\/.*?\//g);
+	// get unique matches (no need to check if it returned any, since it will always return at least one)
+	const unique = matched.filter((el,i,arr) => arr.indexOf(el) === i);
+	// filter out "stamptime" (since all will have this) and replace "node_modules/" and trailing "/"
+	const reduced = unique.reduce((acc, el) => {
+    	const name = el.match(/node_modules\/(.*?)\//);
+    	return (name[1] !== "stamptime" ) ? (acc + "_" + name[1]) : acc;
+	}, (id||0).toString() );
+	return path.join(__dirname, reduced );
+}
 
 module.exports = {
 	get: id => {
